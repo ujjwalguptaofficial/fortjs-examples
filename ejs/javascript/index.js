88257899bc7082cae201486e8312bdd009f9b9f9
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2020-08-01 10:31:50 +0530

    update ejs view engine example

--- ejs/javascript/index.js
+++ ejs/javascript/index.js
@@ -1,16 +1,17 @@
 import * as path from "path";
-import {
-    App
-} from "./app";
+import { Fort } from "fortjs";
+import { routes } from "./routes";
+import { EjsViewEngine } from "./ejs_view_engine";
 
-const app = new App();
-app.create({
-    folders: [{
-        alias: "/",
-        path: path.join(__dirname, "../static")
-    }]
-}).then(() => {
-    console.log("Your fort is located at address - localhost:4000");
+Fort.routes = routes;
+Fort.viewEngine = EjsViewEngine;
+Fort.folders = [{
+    alias: "/",
+    path: path.join(__dirname, "../static")
+}];
+
+Fort.create().then(() => {
+    Fort.logger.info("Your fort is located at address - localhost:4000");
 }).catch(err => {
     console.error(err);
 })
