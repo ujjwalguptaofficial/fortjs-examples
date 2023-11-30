a2a1172982778ba556fad8f17176f52899e1a896
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-11-30 14:10:59 +0400

    update winston example

---
+++ winston/src/index.ts
@@ -0,0 +1,26 @@
+import * as path from "path";
+import { Fort } from "fortjs";
+import { routes } from "@/routes";
+import { logger } from "./logger";
+
+export const createApp = async () => {
+    Fort.folders = [{
+        alias: "/",
+        path: path.join(__dirname, "../static")
+    }];
+
+    Fort.routes = routes;
+
+    Fort.logger = logger;
+    
+    await Fort.create();
+    process.env.APP_URL = `http://localhost:${Fort.port}`;
+};
+if (process.env.NODE_ENV !== "test") {
+    createApp().then(() => {
+        Fort.logger.debug(`Your fort has been forged and is now ready for exploration at ${process.env.APP_URL}`);
+    }).catch(err => {
+        console.error(err);
+    });
+}
+
---
+++ winston/src/logger.ts
@@ -0,0 +1,85 @@
+import { Logger } from 'fortjs'
+import { createLogger, format, transports } from 'winston'
+import 'winston-daily-rotate-file'
+
+const winstonLogger = createLogger({
+    level: 'info',
+    format: format.json(),
+    transports: [
+        //
+        // - Write to all logs with level `info` and below to `combined.log`
+        // - Write all logs error (and below) to `error.log`.
+        //
+        new (transports as any).DailyRotateFile({
+            datePattern: 'DD-MM-YYYY',
+            filename: 'logs/error.log',
+            level: 'error',
+            format: format.combine(
+                format.timestamp({
+                    format: 'YYYY-MM-DD hh:mm:ss a',
+                }),
+            ),
+        }),
+        new (transports as any).DailyRotateFile({
+            datePattern: 'DD-MM-YYYY',
+            filename: 'logs/all.log',
+            level: 'info',
+            format: format.combine(
+                format.timestamp({
+                    format: 'YYYY-MM-DD hh:mm:ss a',
+                }),
+            ),
+        }),
+    ],
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    winstonLogger.add(
+        new transports.Console({
+            format: format.simple(),
+            level: 'debug',
+        }),
+    )
+}
+
+class MyLogger extends Logger {
+    error(...message) {
+        message = message.map((msg) => {
+            if (typeof msg === 'object') {
+                if (msg instanceof Error) {
+                    const err = msg as Error
+                    msg = `error- 
+                    mesage: ${err.message}
+                    name:${err.name},
+                    stack:${err.stack}
+                    `
+                } else {
+                    msg = JSON.stringify(msg)
+                }
+            }
+            return msg
+        })
+        winstonLogger.error(message.join(' '))
+    }
+
+    info(...message) {
+        message = message.map((msg) => {
+            if (typeof msg === 'object') {
+                msg = JSON.stringify(msg)
+            }
+            return msg
+        })
+        winstonLogger.info(message.join(' '))
+    }
+
+    debug(...args) {
+        // winstonLogger.debug(message);
+        console.log(...args)
+    }
+
+    log(level, msg) {
+        winstonLogger.log(level, msg)
+    }
+}
+
+export const logger = new MyLogger()
